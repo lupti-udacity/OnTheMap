614f51ce58e19468798ddee124cba2e4c30aa332
lupti-udacity <lupti.cheung.udacity@gmail.com>
2016-01-04 22:00:01 -0800

    add FBSDK frameworks to the directory

---
+++ OnTheMap/UdacitySession.swift
@@ -0,0 +1,32 @@
+//
+//  UdacitySession.swift
+//  OnTheMap
+//
+//  Created by Lupti on 12/28/15.
+//  Copyright © 2015 lupti. All rights reserved.
+//
+
+import Foundation
+/* Udacity Session Struct */
+
+struct UdacitySession {
+    
+    var registered: Bool?
+    var uniqueKey: String?
+    var id: String?
+    var expiration: NSDate?
+
+    init(dictionary: NSDictionary) {
+        
+        registered = dictionary["registered"] as? Bool
+        uniqueKey = dictionary["uniqueKey"] as? String
+        id = dictionary["id"] as? String
+        expiration = dictionary["expiration"] as? NSDate
+
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "yyy-MM-dd'T'HH:mm:ss'.'SSSZ"
+        if let expirationString = dictionary["expiration"] as? String {
+            expiration = formatter.dateFromString(expirationString)
+        }
+    }
+}
